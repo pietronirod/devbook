6e1b35d4c8aa9dce940307931fe39e9ddb7f1c59
David <david@pietroniro.com.br>
2022-08-12 14:06:37 -0300

    Criado

---
+++ api/sql/sql.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS devbook;
+USE devbook;
+DROP TABLE IF EXISTS usuarios;
+CREATE TABLE usuarios(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    nome varchar(50) NOT NULL,
+    nick varchar(50) NOT NULL UNIQUE,
+    email varchar(50) NOT NULL UNIQUE,
+    senha varchar(20) NOT NULL,
+    criadoEm TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
+) ENGINE=INNODB;
