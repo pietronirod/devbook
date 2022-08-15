07db007255c21e0918ed2922baa056a7a04ea8cf
David <david@pietroniro.com.br>
2022-08-15 16:59:32 -0300

    Criacao do pacote de repositorios

---
+++ api/src/repositorios/usuarios.go
@@ -0,0 +1,13 @@
+package repositorios
+
+import "database/sql"
+
+// Usuarios representa um repositorio de usuarios
+type Usuarios struct {
+	db *sql.DB
+}
+
+// NovoRepositorioDeUsuarios cria um repositorio de usuarios
+func NovoRepositorioDeUsuarios(db *sql.DB) *Usuarios {
+	return &Usuarios{db}
+}
